8f3a7b6bdc7b5bbdf76deb8e444d9571b9da40f5
Michael Ekstrand <michael@elehack.net>
2014-08-18 15:55:56 -0500

    Print item scores

--- src/main/java/org/grouplens/lenskit/hello/HelloLenskit.java
+++ src/main/java/org/grouplens/lenskit/hello/HelloLenskit.java
@@ -134,7 +134,7 @@ public class HelloLenskit implements Runnable {
             List<ScoredId> recs = irec.recommend(user, 10);
             System.out.format("Recommendations for %d:\n", user);
             for (ScoredId item: recs) {
-                System.out.format("\t%d\n", item.getId());
+                System.out.format("\t%d\t%.2f\n", item.getId(), item.getScore());
             }
         }
     }
