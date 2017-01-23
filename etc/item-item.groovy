62b5cc6a31af5d3441836a23394f5f9ef4e8eea4
Michael Ekstrand <michaelekstrand@boisestate.edu>
2017-01-23 11:59:27 -0700

    heap size, model size updates
    This makes 2 changes for memory use:
    - Set a default heap size of 1GB
    - Limit the model size to reduce memory use

--- etc/item-item.groovy
+++ etc/item-item.groovy
@@ -7,12 +7,16 @@ import org.lenskit.baseline.UserMeanBaseline
 import org.lenskit.baseline.UserMeanItemScorer
 import org.lenskit.knn.MinNeighbors
 import org.lenskit.knn.item.ItemItemScorer
+import org.lenskit.knn.item.ModelSize
 
 // ... and configure the item scorer.  The bind and set methods
 // are what you use to do that. Here, we want an item-item scorer.
 bind ItemScorer to ItemItemScorer.class
 // Item-item works best with a minimum neighbor count
 set MinNeighbors to 2
+
+// Limit the model size
+set ModelSize to 1000
 
 // let's use personalized mean rating as the baseline/fallback predictor.
 // 2-step process:
